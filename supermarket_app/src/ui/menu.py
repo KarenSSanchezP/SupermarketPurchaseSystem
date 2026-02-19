994575600f9c41b5c20991c3fd625e3861fc8257
Karen S. Sánchez <112920282+KarenSSanchezP@users.noreply.github.com>
2026-02-19 02:14:07 -0600

    add: Mejora del menú principal y de las opciones de administrador y cliente

--- supermarket_app/src/ui/menu.py
+++ supermarket_app/src/ui/menu.py
@@ -1,3 +1,4 @@
+import os, time
 from supermarket_app.src.services.auth_service import AuthService
 
 class Menu:
@@ -24,18 +25,20 @@ class Menu:
         Muestra el menú principal y maneja la selección del usuario
         """
         while True: # Bucle principal del menú
-            print("=" * 40)
-            print("Supermarket Purchase System")
-            print("=" * 40)
+            print("=" * 43)
+            print("\tSupermarket Purchase System")
+            print("=" * 43)
             
             for opcion in self.opciones_inicio:
-                print(opcion)
+                print("\t"+opcion)
             opcion = input("Seleccione una opción: ")
             
             if opcion == "1":
                 self.inicio_sesion()
             elif opcion == "2":
-                print("Hasta pronto!")
+                print("-" * 43)
+                print("Gracias por visitarnos. ¡Hasta pronto!\n")
+                time.sleep(2)
                 break
             else:
                 print("Opción no válida")
@@ -44,13 +47,57 @@ class Menu:
         """
         Muestra el menú de administrador y maneja la selección del usuario
         """
-        pass
+        while True: # Bucle del menú de administrador
+            print("=" * 40)
+            print(f"\tPanel de administrador - {usuario_logueado.username}")
+            print("=" * 40)
+            
+            for opcion in self.opciones_admin:
+                print("\t" + opcion)
+            opcion = input("Seleccione una opción: ")
+            
+            if opcion == "1":
+                self.menu_admin_ver_productos(usuario_logueado)
+            elif opcion == "2":
+                self.menu_admin_añadir_producto(usuario_logueado)
+            elif opcion == "3":
+                self.menu_admin_eliminar_producto(usuario_logueado)
+            elif opcion == "4":
+                print("-" * 43)
+                print("Cerrando sesión del administrador...")
+                time.sleep(2)
+                os.system("cls" if os.name == "nt" else "clear")
+                break
+            else:
+                print("Opción no válida. Vuelve a intentarlo.")
     
     def menu_cliente(self, usuario_logueado):
         """
         Muestra el menú de cliente y maneja la selección del usuario
         """
-        pass
+        while True: # Bucle del menú de cliente
+            print("\n" + "=" * 40)
+            print(f"\tPanel de cliente - {usuario_logueado.username}")
+            print("=" * 40)
+            
+            for opcion in self.opciones_cliente:
+                print("\t" + opcion)
+            opcion = input("Seleccione una opción: ")
+            
+            if opcion == "1":
+                self.menu_cliente_ver_productos(usuario_logueado)
+            elif opcion == "2":
+                self.menu_cliente_comprar_productos(usuario_logueado)
+            elif opcion == "3":
+                self.menu_cliente_ver_carrito(usuario_logueado)
+            elif opcion == "4":
+                print("-" * 43)
+                print("Cerrando sesión del cliente...\n")
+                time.sleep(2)
+                os.system("cls" if os.name == "nt" else "clear")
+                break
+            else:
+                print("Opción no válida. Vuelve a intentarlo.")
     
     def inicio_sesion(self):
         """
@@ -59,12 +106,12 @@ class Menu:
         servicio_auth = AuthService()
         while True: # Bucle de inicio de sesión
             try:
-                print("\n--- INICIAR SESIÓN ---" 
+                print("\n" + "-" * 14 + " INICIAR SESIÓN " + "-" * 14,
                     "\nEscriba '0' en usuario para salir del inicio de sesión")
                 
-                usuario_input = input("Ingrese su usuario: ")
+                usuario_input = input(">>> Ingrese su usuario: ")
                 if usuario_input == "0": return
-                contrasena_input = input("Ingrese su contraseña: ")
+                contrasena_input = input(">>> Ingrese su contraseña: ")
                 
                 usuario_logueado = servicio_auth.login(usuario_input, contrasena_input)
                 print(f"¡Bienvenido {usuario_logueado.username}!")
